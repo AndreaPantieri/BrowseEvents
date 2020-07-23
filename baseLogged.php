61b762c1287b788ec5df513cfb963a3006e1f190
Andrea Pantieri <andrea.pantieri98@gmail.com>
2020-07-23 17:34:43 +0200

    adding links in baseLogged

--- baseLogged.php
+++ baseLogged.php
@@ -1,32 +1,36 @@
 <!-- Topbar -->
-    <div id="topbar"></div>
+<div id="topbar"></div>
 
-    <!-- Search Bar -->
-    <div id="searchbar"></div>
+<!-- Search Bar -->
+<div id="searchbar"></div>
 
-    <!-- Main content -->
-    <div id="maincontent">
-        
-    </div>
+<!-- Main content -->
+<div id="maincontent">
+    
+</div>
 
-    <!-- Footbar -->
-    <div id="footbar"></div>
+<!-- Footbar -->
+<div id="footbar"></div>
 
-    <!-- Slide Menu -->
-    <div id="slidemenu">
-        <ol>
-            <li class="subslidemenu">
-                <h4>Categories</h3>
-                <ol>
-                    <li>Concerts</li>
-                    <li>Exhibitions</li>
-                    <li>Festivals</li>
-                </ol>
-            </li>
-            <li>My account</li>
-            <li>Create new event</li>
-            <li>Manage events</li>
-            <li>My orders</li>
-            <li>Notifications</li>
-        </ol>
-    </div>
+<!-- Slide Menu -->
+<div id="slidemenu">
+    <ol>
+        <li class="subslidemenu">
+            <h4>Categories</h3>
+            <ol>
+                <li id="concerts">Concerts</li>
+                <li id="exhibitions">Exhibitions</li>
+                <li id="festivals">Festivals</li>
+            </ol>
+        </li>
+        <li id="my_account">My account</li>
+        <li id="new_event">Create new event</li>
+        <li id="manage_events">Manage events</li>
+        <li id="my_orders">My orders</li>
+        <li id="notifications">Notifications</li>
+    </ol>
+</div>
+
+<script type="text/javascript">
+    
+</script>
