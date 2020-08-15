6b4e7170d92247f839ab931c7d3e3cb26d8e7513
Andrea Pantieri <andrea.pantieri98@gmail.com>
2020-08-16 00:06:58 +0200

    starting form new event

--- newevent.php
+++ newevent.php
@@ -1,1 +1,9 @@
-<h1>New Event</h1>
+<form id="form-newevent">
+	<h1 id="title-newevent">New Event</h1>
+	
+	<div class="form-group" style="margin: auto;">
+		<input type="file" id="selectImage" style="display: none;">
+		<input type="button" class="btn btn-primary mb-2" value="Load a new photo" onclick="document.getElementById('selectImage').click();" />
+		<button type="button" class="btn btn-primary mb-2">Remove selected</button>
+	</div>
+</form>
