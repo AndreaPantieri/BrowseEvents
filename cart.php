6e4a4754656210715b700746eebfbe4c399caa50
Andrea <artolink97@gmail.com>
2020-08-25 03:23:56 +0200

    almost finished cart

--- cart.php
+++ cart.php
@@ -1,19 +1,42 @@
 <div class="container-fluid">
     <div class="row px-5">
         <div class="col-md-7">
-            <div class="shopping-cart">
+            <div class="mycart">
                 <h6>My Cart</h6>
             </div>
             <hr>
             <div id="cart-elements"></div>
         </div>
-    </div>
-</div>
 
-<script>
-    includeContent("php/getCartElements.php", (r) => {
-        var res = JSON.parse(r);
-        var html = res["HTML"];
-        document.getElementById("cart-elements").innerHTML = html;
-    });
-</script>
+        <div class="col-md-4 offset-md-1 border rounded mt-5 bg-white h-25">
+            <div class="pt-4">
+                <h6>PRICE DETAILS</h6>
+                <hr>
+                <div class="row price-details">
+                    <div class="col-md-6">
+                        <?php
+                        //if (isset($_SESSION['cart'])) {
+                        //echo "<h6>Price ($_SESSION["cart"] items)</h6>";
+                        //} else {
+                        echo "<h6>Price (0 items)</h6>";
+                        // }
+                        ?>
+                    </div>
+                </div>
+                <div id="total">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script>
+        $.ajax({
+            type: "GET",
+            url: "php/getCartElements.php"
+        }).then(function(data) {
+            var tmp = JSON.parse(data);
+            document.getElementById("cart-elements").innerHTML = tmp["HTML"];
+            document.getElementById("total").innerHTML = "Total: " + tmp["total"] + "€";
+        });
+    </script>
--- php/getCartElements.php
+++ php/getCartElements.php
@@ -1,51 +1,53 @@
 <?php
 require_once 'DBHandler.php';
 
-class getCartElementsResponse
-{
+class getCartElementsResponse{
+    public $total = 0;
     public $HTML = "<div class='cart-element'><h1>Your cart is empty!</h1></div>";
 }
-
-$DBHandler = new DBHandler();
 $getCartElementsResponse = new getCartElementsResponse();
+$DBHandler = new DBHandler();
 
 if (isset($_SESSION["userid"])) {
     $userid = $_SESSION["userid"];
 
-    $sql = "SELECT cart.TicketQuantity, event.Name, event.Price, event.Image, user.Username, cart.Description FROM cart 
+    $sql = "SELECT cart.TicketQuantity, event.Name, event.Price, event.Image, user.Username, image.Description FROM cart 
     INNER JOIN event ON cart.Event_idEvent = event.idEvent 
     INNER JOIN user ON cart.User_idUsers = user.idUsers 
-    INNER JOIN image ON image.Event_idEvent  = event.idEvent 
+    INNER JOIN image ON image.Event_idEvent = event.idEvent 
     WHERE cart.User_idUsers = '$userid' AND isAcquired = 'false'";
     $result = $DBHandler->select($sql);
 
     if ($result) {
-        foreach ($result as $row) {
-            $HTML .= '
-        <form action="cart.php" method="GET" class="cart-items">
-            <div class="border rounded">
-                <div class="row bg-white">
-                    <div class="col-md-3">
-                        <img src="$row[event.Image]" alt="$row[cart.Description]" class="img-fluid">
-                    </div>
-                    <div class="col-md-6">
-                        <h5 class="pt-2">$row[event.Name]</h5>
-                        <small class="text-secondary">$row[user.Username]</small>
-                        <h5 class="pt-2">$row[event.Price]</h5>
-                    </div>
-                    <div class="col-md-5 py-3">
-                        <div>
-                            <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-minus"></i></button>
-                            <input type="text" value="$row[cart.TicketQuantity]" class="form-control w-25 d-inline">
-                            <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-plus"></i></button>
-                            <button type="submit" class="btn btn-danger mx-2" name="remove">Remove</button>
+        $counts = array_map('count', $result);
+        if (count($counts) != 0) {
+            $getCartElementsResponse->HTML = "";
+
+            foreach ($result as $row) {
+                $getCartElementsResponse->total += $row["Price"];
+                $getCartElementsResponse->HTML .= '
+                <div class="border rounded">
+                    <div class="row bg-white">
+                        <div class="col-md-3">
+                            <img src="' . $row["Image"] . '" alt="' . $row["Description"] . '" class="img-fluid">
+                        </div>
+                        <div class="col-md-6">
+                            <h5 class="pt-2">' . $row["Name"] . '</h5>
+                            <small class="text-secondary">Organizer: ' . $row["Username"] . '</small>
+                            <h5 class="pt-2">Price: ' . $row["Price"] . '€</h5>
+                        </div>
+                        <div class="col-md-5 py-3">
+                            <div>
+                                <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-minus"></i></button>
+                                <input type="text" value="' . $row["TicketQuantity"] . '" class="form-control w-25 d-inline">
+                                <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-plus"></i></button>
+                                <button type="submit" class="btn btn-danger mx-2" name="remove">Remove</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </form>';
+                </div>';
+            }
         }
     }
 }
-
 echo json_encode($getCartElementsResponse);
