a74fef1a33636b3235f2a925aafb8184a4b94338
Andrea Pantieri <andrea.pantieri98@gmail.com>
2020-08-27 19:52:06 +0200

    adding script php for events in home

--- events.php
+++ events.php
@@ -1,22 +1,29 @@
 <div id="events-display">
+	<?php
+	require_once 'php/DBHandler.php';
+	$DBHandler = new DBHandler();
+	$last_id = 0;
+
+	if(isset($_SESSION["event_last_id"])){
+		$last_id = $_SESSION["event_last_id"];
+	}
+	
+	$sql_e = "SELECT idEvent, Name, Datetime FROM `event` WHERE idEvent > $last_id ORDER BY Datetime DESC LIMIT 5 ";
+	$result = $DBHandler->select($sql_e);
+
+	foreach ($result as $var) {
+		?>
 	<div class="event-container card mb-3">
-		<img class="event-image card-img-top img-fluid" src="./res/img/events/12/0.jpg"/>
+		<img class="event-image card-img-top img-fluid" <?php echo 'src="./res/img/events/' . $var["idEvent"] .'/0.jpg"'; ?>/>
 		<div class="event-text card-body">
-			<div class="event-title card-title">Event name</div>
-			<div class="event-date card-text">Event date</div>
+			<div class="event-title card-title"><?php echo $var["Name"];?></div>
+			<div class="event-date card-text"><?php echo $var["Datetime"];?></div>
 			<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right event-arrow" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 			  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
 			</svg>
 		</div>	
 	</div>
-	<div class="event-container card mb-3">
-		<img class="event-image card-img-top img-fluid" src="./res/img/events/12/0.jpg"/>
-		<div class="event-text card-body">
-			<div class="event-title card-title">Event name</div>
-			<div class="event-date card-text">Event date</div>
-			<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right event-arrow" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-			  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-			</svg>
-		</div>	
-	</div>
+		<?php
+	}
+	?>
 </div>
