c67a78367749ac38ea0f6b97995d92263513ad45
Andrea Pantieri <andrea.pantieri98@gmail.com>
2020-08-27 18:27:27 +0200

    adding events to home

--- events.php
+++ events.php
@@ -1,1 +1,22 @@
-<h1>EVENTS</h1>
+<div id="events-display">
+	<div class="event-container card mb-3">
+		<img class="event-image card-img-top img-fluid" src="./res/img/events/12/0.jpg"/>
+		<div class="event-text card-body">
+			<div class="event-title card-title">Event name</div>
+			<div class="event-date card-text">Event date</div>
+			<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right event-arrow" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+			  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+			</svg>
+		</div>	
+	</div>
+	<div class="event-container card mb-3">
+		<img class="event-image card-img-top img-fluid" src="./res/img/events/12/0.jpg"/>
+		<div class="event-text card-body">
+			<div class="event-title card-title">Event name</div>
+			<div class="event-date card-text">Event date</div>
+			<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right event-arrow" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+			  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+			</svg>
+		</div>	
+	</div>
+</div>
