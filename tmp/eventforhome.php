0c54d07d44cd4ebbfd3455d1aea642d58a6dbdcd
Andrea Pantieri <andrea.pantieri98@gmail.com>
2020-08-27 18:01:40 +0200

    template for events in home

---
+++ tmp/eventforhome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>TITOLO</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap CSS + JS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="test.css">
+</head>
+
+<body>
+	<div class="event-container card mb-3">
+		<img class="event-image card-img-top img-fluid" src="../res/img/events/12/0.jpg"/>
+		<div class="event-text card-body">
+			<div class="event-title card-title">Event name</div>
+			<div class="event-date card-text">Event date</div>
+		</div>	
+	</div>
+</body>
+</html>
